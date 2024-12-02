ff9bc82150061ad5b1c814d51535ee8cfda75148
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-12-02 17:32:33 -0300

    Finished

--- src/18_reverse/index.ts
+++ src/18_reverse/index.ts
@@ -1,4 +1,4 @@
-// O método reverse() altera o array original e cria um novo array com os elementos invertidos.
+// O método reverse() altera o array original e cria um novo array com a posição dos elementos invertidos.
 const array23: number[] = [6, 5, 4, 3 , 2, 1];
 const reversedArray23: number[] = array23.reverse();
 console.log(reversedArray23); // [1, 2, 3, 4, 5, 6]
