cb568a0434fb9381d7bef982f94ba0943574a882
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-12-02 16:37:11 -0300

    Lesson 9

---
+++ src/9_pop/index.ts
@@ -0,0 +1,7 @@
+// O método pop() remove o último elemento do array e retorna ele
+
+const array10: number[] = [10, 80, 230, 90, 20, 49, 230];
+
+const lastElement = array10.pop();
+console.log(array10); // [10, 80, 230, 90, 20]
+console.log(lastElement); // 230
