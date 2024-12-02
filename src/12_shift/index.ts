84c9f88e1e53e5621f8901e1321a04e59228e63d
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-12-02 16:52:39 -0300

    Lesson 12

---
+++ src/12_shift/index.ts
@@ -0,0 +1,4 @@
+// O método shift() remove o primeiro elemento do array e retorna o elemento removido.
+const array14: number[] = [10, 20, 30, 40, 50];
+const firstElement = array14.shift();
+console.log(firstElement); // 10
