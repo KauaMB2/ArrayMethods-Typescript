0f4f59c9bd894d2616cc6f9378d3977bf1660686
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-12-02 17:00:41 -0300

    Lesson 14

---
+++ src/14_some/index.ts
@@ -0,0 +1,3 @@
+// O método some() retorna true se pelo menos um elemento do array satisfaz a condição especificada, e false caso nenhum elemento satisfaz a condição.
+const array16: number[] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+console.log(array16.some((element) => element > 50));
